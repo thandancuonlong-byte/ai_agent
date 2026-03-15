d103ab7ace86ae6b0276659ecbf6972a4042261c
thandancuonlong-byte <thandancuonlong@gmail.com>
2026-03-15 19:15:44 +0700

    Update screen_capture.py

--- perception/screen_capture.py
+++ perception/screen_capture.py
@@ -1,1 +1,13 @@
+import pyautogui
 
+
+class ScreenCapture:
+
+    def capture(self):
+
+        # capture screen
+        screenshot = pyautogui.screenshot()
+
+        print("Screen captured")
+
+        return screenshot
